b5cfeb7f91ae4cda344315da213284f90022fefa
isaacglezacosta <isaacglezacosta10@gmail.com>
2023-12-22 21:28:21 -0600

    Programas para cifrar y descifrar cifrado César

---
+++ Cifrado César_cifrar.py
@@ -0,0 +1,14 @@
+texto = input("Ingrese el texto a cifrar: ")
+desplazamiento = int(input("Ingrese el número del desplazamiento: "))
+abecedario = "abcdefghijklmnñopqrstuvwxyz"
+texto_cifrado = ""
+
+for caracter in texto:
+    if caracter == " ":
+        texto_cifrado += " "
+    else:
+        posicion = abecedario.find(caracter)
+        posicion_nueva = posicion + desplazamiento
+        texto_cifrado += abecedario[posicion_nueva]
+
+print(f"Texto cifrado: {texto_cifrado}")
